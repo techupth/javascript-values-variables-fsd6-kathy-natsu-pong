432302260ca66f3bc1a51e8e914b8538a5073ed7
Pong <pongpong34756@gmail.com>
2024-05-09 14:43:03 +0700

    feat:value and variable

--- ex-1and2.js
+++ ex-1and2.js
@@ -3,3 +3,18 @@
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+let firstname = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalBillCost = 15000;
+const totalOpeningReastaurant = 1500;
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBillCost = 5000;
+totalOpeningReastaurant = 2500
+
+
+
