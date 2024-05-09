2ea59243bfbd560e2f88018a2b8281ffd652a5b5
KathyKZ <kathykate.sooksutum@gmail.com>
2024-05-09 14:38:50 +0700

    feat: add JS

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstname = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const netBillAmount = 15000;
+const numberOfRestaurantOpen = 1500;
+
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netBillAmount = 5000;
+numberOfRestaurantOpen = 2500;
+
